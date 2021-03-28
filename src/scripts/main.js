8219dd59a3f7237353b207af49f9d4eb56373075
Taras Chirkov <chirkovtaras@gmail.com>
2021-03-28 21:23:37 +0300

    added popup and fix froms

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,3 +9,21 @@ navBurder.onclick = function() {
   menu.classList.toggle('side-info_active');
   menuLink.classList.toggle('menu__list_tablet_active');
 };
+
+const modal = document.getElementById('myModal');
+const btn = document.querySelector('.popup-button');
+const span = document.querySelector('.close');
+
+btn.onclick = function() {
+  modal.style.display = 'block';
+};
+
+span.onclick = function() {
+  modal.style.display = 'none';
+};
+
+window.onclick = function(event) {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+};
